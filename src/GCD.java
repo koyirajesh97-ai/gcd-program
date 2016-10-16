8521f03e25769ecce5c0e170aab51f6b7cf1b57e
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-10-16 15:14:31 +0000

    changes

--- src/GCD.java
+++ src/GCD.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class GCD {
     public static void main(String[] args){
+    // testing
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
