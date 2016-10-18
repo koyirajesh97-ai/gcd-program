b4b1ee6f87c7cc278d09d92f32c9a892cd7578d0
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-10-18 10:38:36 +0000

    added note

--- src/GCD.java
+++ src/GCD.java
@@ -4,6 +4,7 @@ public class GCD {
     public static void main(String[] args){
     // testing
         Scanner sc = new Scanner(System.in);
+        // added exception check
         int a = sc.nextInt();
         int b = sc.nextInt();
         while(b != 0){
