dcad56748b42f0d9dfa2591d846172f613fe56e9
Rajesh Koyi <285258343+koyirajesh97-ai@users.noreply.github.com>
2016-12-17 20:41:52 +0000

    added ref

--- src/GCD.java
+++ src/GCD.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class GCD {
     public static void main(String[] args){
+    // ref: geeksforgeeks
     // updated
     // testing
         Scanner sc = new Scanner(System.in);
